8a159ca6a30077c580026e95ecdf97452a025ec8
JP-Grimaldi <joaopedrogrimaldi@gmail.com>
2026-05-22 09:56:45 -0300

    Aula 2 - Cria servidor com Express

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("Servidor iniciado");
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Página inicial com Express');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('Página de sobre com Express');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('Página de contato com Express');
+});
+
+app.get('/produtos', (req, res) => {
+    res.send('Lista de produtos');
+});
+
+app.get('/servicos', (req, res) => {
+    res.send('Lista de servicos');
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+})
